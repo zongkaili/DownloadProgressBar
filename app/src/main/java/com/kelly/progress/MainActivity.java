3fccb8057a7b4f012a724f3ac3da1cf9aa302fac
zongkaili <zongkaili@idealsee.cn>
2017-03-06 22:33:52 +0800

    增加下载按钮

--- app/src/main/java/com/kelly/progress/MainActivity.java
+++ app/src/main/java/com/kelly/progress/MainActivity.java
@@ -5,20 +5,22 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.kelly.progressbar.FlikerProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    FlikerProgressBar flikerProgressBar;
+    private FlikerProgressBar mFlikerProgressBar;
+    private Button mDownloadBtn;
 
     Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            flikerProgressBar.setProgress(msg.arg1);
+            mFlikerProgressBar.setProgress(msg.arg1);
             if(msg.arg1 == 100){
-                flikerProgressBar.finishLoad();
+                mFlikerProgressBar.finishLoad();
             }
         }
     };
@@ -27,20 +29,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        flikerProgressBar = (FlikerProgressBar) findViewById(R.id.flikerbar);
+        mFlikerProgressBar = (FlikerProgressBar) findViewById(R.id.flikerbar);
+        mDownloadBtn = (Button) findViewById(R.id.download);
 
-        flikerProgressBar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!flikerProgressBar.isFinish()){
-                    flikerProgressBar.toggle();
-                }
-            }
-        });
+        mFlikerProgressBar.setOnClickListener(this);
+        mDownloadBtn.setOnClickListener(this);
 
-        downLoad();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.flikerbar:
+                if(!mFlikerProgressBar.isFinish()){
+                    mFlikerProgressBar.toggle();
+                }
+                break;
+            case R.id.download:
+                downLoad();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    /**
+     * 模拟下载
+     */
     private void downLoad() {
         new Thread(new Runnable() {
             @Override
